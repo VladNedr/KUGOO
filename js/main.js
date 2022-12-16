82521854084e5b7706fd54c0c9a0bc163189a579
VladNedr <nedr2517@mail.ru>
2022-12-16 19:29:49 +0300

    add: mobile menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+const mMenuToggle = document.querySelector(".mobile-menu-toggle");
+const menu = document.querySelector(".mobile-menu");
+
+const openMenu = (event) => {
+  // Функция открывания меню
+  menu.classList.add("is-open"); // Вешает класс is-open
+  mMenuToggle.classList.add("close-menu");
+  document.body.style.overflow = "hidden"; // Запрещает прокрутку сайта под меню
+  lightModeOn();
+};
+const closeMenu = (event) => {
+  // Функция закрывания меню
+  menu.classList.remove("is-open"); // убирает класс is-open
+  mMenuToggle.classList.remove("close-menu");
+  document.body.style.overflow = ""; // возвращает прокрутку сайта под меню
+  lightModeOff();
+};
+
+
+mMenuToggle.addEventListener("click", (event) => {
+  event.preventDefault();
+  menu.classList.contains("is-open") ? closeMenu() : openMenu();
+});
+// Если меню имеет класс is-open открой меню,если нет,то закрой!
+
 const swiper = new Swiper(".swiper", {
   speed: 400,
   slidesPerView: 1,
